4cc5434d78b474c1835ce17cd4b2ec83fcfa7e1a
rau4o <rkoshkinbayev@gmail.com>
2019-11-15 03:31:40 +0600

    Initial Commit

---
+++ CryptoTracer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoTracer
+//
+//  Created by rau4o on 11/15/19.
+//  Copyright © 2019 rau4o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
